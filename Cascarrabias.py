f60e4732c0390df5a9ec8a25bfb2fbc33e335e66
Alex-TRABAJO <alex@snt.es>
2020-08-25 10:56:32 +0200

    Añadido sistema simple de memoria, hay que seguir trabajándolo

--- Cascarrabias.py
+++ Cascarrabias.py
@@ -1,12 +1,25 @@
 import discord
 import random
+import asyncio
 import sys
+import Recuerdos
 
 
 BOOMER_FILE = open("boomers.txt", "r")
 BOOMER_IMGS = BOOMER_FILE.readlines()
 BOOMER_FILE.close()
 
+# Añadimos DEBUG para tracing en los errores
+DEBUG = True if sys.argv[2] == "True" else False
+
+if DEBUG == True:
+    print("[*] Iniciando Cascarrabias en modo DEBUG.")
+
+# Cargamos la memoria
+memoria = Recuerdos.Memoria("memoria.db")
+
+
+# Marcamos si el archivo de imágenes boomers se puede usar
 if len(BOOMER_IMGS) > 0:
     CHISTES_BOOMERS = True
 else:
@@ -21,23 +34,96 @@ try:
     async def on_ready():
         await client.get_channel(688816974947811406).send("Estúpidos críos, me han despertado de la siesta jugando al balón.")
 
+        # Intentamos cargar la memoria del bot
+        try:
+            if memoria.ready:
+                # No hacemos nada para no floodear
+                pass
+            else:
+                # Si no creamos una excepción y desactivamos la memoria
+                raise "Error cargando la memoria"
+            if DEBUG == True:
+                await client.get_channel(688816974947811406).send("Estoy en modo debug, así que puede que diga cosas raras.")
+        except Exception:
+            await client.get_channel(688816974947811406).send("Hoy no me acuerdo de nada, es como si me hubiesen formateado el disco duro....")
+            
+
+
 
     @client.event
     async def on_message(message):
+
+        def check(m):
+            # Función que comprueba los mensajes del mismo usuario.
+             return m.author == message.author
+
+
+        channel = message.channel
+
         if message.author == client.user:
+            # Desactivamos que el BOT se pueda auto-contestar para no spamear.
             return
-        if message.content.startswith("Eh, viejo") or message.content.startswith("eh, viejo") or message.content.startswith("eh, Viejo") or message.content.startswith("Eh, Viejo"):
-            await message.channel.send("¿Qué quieres ahora? {0}".format(str(message.author).split("#")[0]))
-        if "cuentame chiste" in str(message.content) or "dime chiste" in str(message.content):
-            if CHISTES_BOOMERS:
-                await message.channel.send("Ja, ja... Mira que gracioso el chiste que me pasó mi cuñado el otro día por Whatsapp. {0}".format(random.choice(BOOMER_IMGS)))
-            else:
-                await message.channel.send("Ahora mismo no me acuerdo de ningún chiste...")
-        if "otro chiste" in str(message.content):
-            if CHISTES_BOOMERS:
-                await message.channel.send("Vaya, parece que tenemos al graciosillo...")
-            else:
-                await message.channel.send("Ahora mismo no me acuerdo de ningún chiste...")
+
+        else:
+            # Procedemos con el funcionamiento normal del BOT
+            if "eh, viejo" in message.content.lower():
+                await channel.send("¿Qué quieres ahora? {0}".format(str(message.author).split("#")[0]))
+
+                # Esperamos la respuesta del usuario que ha llamado al bot.
+                try:
+
+                    message = await client.wait_for("message", timeout=30.0, check=check)
+
+                    # Hook de contar chistes
+                    if "cuentame un chiste" in message.content.lower() or "dime un chiste" in message.content.lower() or "dime otro chiste" in message.content.lower():
+                        if CHISTES_BOOMERS:
+                            await channel.send("Ja, ja... Mira que gracioso el chiste que me pasó mi cuñado el otro día por Whatsapp. {0}".format(random.choice(BOOMER_IMGS)))
+                        else:
+                            await channel.send("Ahora mismo no me acuerdo de ningún chiste...")
+
+                    # Hook de guardar gustos
+                    elif "me gusta" in message.content.lower():
+                        if memoria.ready:
+                            interes = message.content.replace("me gusta", "")
+                            guardado = memoria.guardar_interes_usuario(message.author, interes)
+                            if guardado == True:
+                                await channel.send("Vale, me acordaré de que te gusta {0}".format(interes.strip()))
+                            else:
+                                if DEBUG == True:
+                                    await channel.send("Se me ha frito el cerebro, no me voy a acordar de eso, dile a los que me crearon esto: {0}".format(guardado))
+                                else:
+                                    await channel.send("Se me ha frito el cerebro, no me voy a acordar de eso.")
+
+                        else:
+                            await channel.send("Ya te voy avisando de que no me voy a acordar de la chorrada esa que me has dicho...")
+
+
+                    # Hook de juegos (muy a la larga, quiero hacer juegos interesantes)
+                    elif "jugar" in message.content.lower() or "jugamos a algo" in message.content.lower():
+                        await channel.send("Todavía estoy aprendiendo juegos, ya jugaremos a algo otro día.")
+
+
+                    # Hook de gustos
+                    elif "estoy triste" in message.content.lower():
+                        if memoria.ready:
+                            intereses = memoria.obtener_intereses_usuario(message.author)
+                            await channel.send("Bueno, siempre puedes ir a {0}, me acuerdo que te gusta mucho".format(random.choice(intereses)))
+                        else:
+                            await channel.send("Pues ala, una palmadita en la espalda y a seguir.")
+
+                    elif "olvida mi nombre, mi cara, mi casa, y pega la vuelta" in message.content.lower():
+                        if memoria.ready:
+                            memoria.limpiar_datos_usuario(message.author)
+                            await channel.send("¡Jamás te pude comprender! A partir de ahora ya no me acordaré de nada de lo que me dijiste.")
+                        else:
+                            await channel.send("Vaya, si parece que sabe la canción de Pimpinela y todo...")
+
+                except asyncio.TimeoutError:
+
+                    # Si no ha contestado en 30 segundos lo mandamos a la mierda suavemente.
+                    await channel.send("¿Qué divertido no? Primero me hablas y después me haces el mismo vacío que te hacen a ti por whatsapp.")
+
+
         
     client.run(TOKEN)
 
---
+++ Recuerdos.py
@@ -0,0 +1,92 @@
+import sqlite3
+import threading
+
+
+class Memoria():
+    """
+    Inicia la memoria de Cascarrabias, para ello habrá que tener una base de datos creada previamente con las tablas definidas en el Readme, si no, no funcionará correctamente
+    """
+    def __init__(self, archivo):
+        try:
+            self.conn = sqlite3.connect(archivo)
+            self.cursor = self.conn.cursor()
+            self.ready = True if self.cursor != None else False
+            print("[*] Iniciada la memoria de Cascarrabias.")
+
+        except Exception as e:
+            print("[*] No se ha podido iniciar la memoria de Cascarrabias")
+        
+
+    def obtener_intereses_usuario(self, id_usuario):
+        """
+        Obtiene los intereses que ha definido el usuario hablando con Cascarrabias y han sido guardados en la base de datos.
+        """
+        intereses = self.cursor.execute("SELECT intereses FROM discord_users WHERE id_usuario = ?", (str(id_usuario),))
+
+        intereses = self.cursor.fetchone()
+
+        if intereses:
+            # Obtenemos los intereses si hay alguno y los devolvemos
+            return intereses[0].split(",")
+        else:
+            # No devolvemos ningún interés
+            return None
+        
+
+    def guardar_interes_usuario(self, id_usuario, interes):
+        """
+        Guarda un nuevo interés para el usuario especificado en la base de datos.
+        """
+        try:
+            intereses = self.cursor.execute("SELECT intereses FROM discord_users WHERE id_usuario = ?", (str(id_usuario),))
+
+            intereses = self.cursor.fetchone()
+        
+            if intereses[0] == None:
+                # Si no hay ningún interés lo guardamos simplemente.
+                self.cursor.execute("UPDATE discord_users SET intereses = ? WHERE id_usuario = ?", ("{0}".format(interes), str(id_usuario)))
+                self.conn.commit()
+                return True
+
+            else:
+                # Separamos los intereses que estarán formateados con una coma si los hubiese y los guardamos en un array.
+                array_intereses = intereses[0].split(",")
+                # Guardamos el nuevo interés en el array.
+                array_intereses.append(interes)
+
+                # Actualizamos la base de datos con el nuevo interés para el usuario.
+                self.cursor.execute("UPDATE discord_users SET intereses = ? WHERE id_usuario = ?", (",".join(array_intereses), str(id_usuario)))
+                self.conn.commit()
+                return True
+
+        except Exception as e:
+            print("[*] Error guardando interés: {0}".format(e))
+            return e
+
+
+
+    def obtener_saludo_usuario(self, id_usuario):
+        """
+        Obtiene el saludo personal para el usuario.
+        """
+        return True
+
+
+    def limpiar_datos_usuario(self, id_usuario):
+        """
+        Limpia todos los datos asociados al usuario especificado de la base de datos.
+        """
+        id_usuario = str(id_usuario)
+
+        self.cursor.execute("UPDATE discord_users SET intereses = NULL, gustos = NULL, saludo_personalizado = NULL, cumpleaños = NULL, provincia = NULL WHERE id_usuario = ?", (id_usuario,))
+        self.conn.commit()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    Memo = Memoria("memoria.db")
+    guardado = Memo.guardar_interes_usuario("xRayO#8858", "la pizza con piña")
